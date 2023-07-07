7e060f1f0bdc24737752a772e57a0fe6de25e01d
lucidlynxx <dzakysyahrizal04@gmail.com>
2023-07-07 17:01:06 +0700

    refactoring database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,6 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use App\Models\Penilaian;
-use App\Models\Alternatif;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -24,119 +22,9 @@ class DatabaseSeeder extends Seeder
             'is_admin' => 1
         ]);
 
-        Alternatif::create([
-            'user_id' => '1',
-            'kodeAlternatif' => 'A1',
-            'slug' => 'A1',
-            'namaAlternatif' => 'Viewsonic Elite XG270'
-        ]);
-        Alternatif::create([
-            'user_id' => '1',
-            'kodeAlternatif' => 'A2',
-            'slug' => 'A2',
-            'namaAlternatif' => 'Philips 325M2CR2'
-        ]);
-        Alternatif::create([
-            'user_id' => '1',
-            'kodeAlternatif' => 'A3',
-            'slug' => 'A3',
-            'namaAlternatif' => 'LG 27UL500-W'
-        ]);
-        Alternatif::create([
-            'user_id' => '1',
-            'kodeAlternatif' => 'A4',
-            'slug' => 'A4',
-            'namaAlternatif' => 'Dell P2422H'
-        ]);
-        Alternatif::create([
-            'user_id' => '1',
-            'kodeAlternatif' => 'A5',
-            'slug' => 'A5',
-            'namaAlternatif' => 'MSI Optix G273'
-        ]);
-        Alternatif::create([
-            'user_id' => '1',
-            'kodeAlternatif' => 'A6',
-            'slug' => 'A6',
-            'namaAlternatif' => 'Samsung Odyssey G5'
-        ]);
+        $this->call(AlternatifSeeder::class);
 
-        Penilaian::create([
-            'user_id' => '1',
-            'alternatif_id' => '1',
-            'slug' => 'Viewsonic-Elite-XG270',
-            'C1x' => '2',
-            'C2x' => '2',
-            'C3x' => '3',
-            'C4x' => '5',
-            'C5x' => '3',
-            'C6x' => '4',
-            'C7x' => '2',
-        ]);
-
-        Penilaian::create([
-            'user_id' => '1',
-            'alternatif_id' => '2',
-            'slug' => 'Philips-325M2CR2',
-            'C1x' => '2',
-            'C2x' => '3',
-            'C3x' => '4',
-            'C4x' => '3',
-            'C5x' => '2',
-            'C6x' => '4',
-            'C7x' => '3',
-        ]);
-
-        Penilaian::create([
-            'user_id' => '1',
-            'alternatif_id' => '3',
-            'slug' => 'LG-27UL500-W',
-            'C1x' => '2',
-            'C2x' => '2',
-            'C3x' => '5',
-            'C4x' => '5',
-            'C5x' => '1',
-            'C6x' => '1',
-            'C7x' => '2',
-        ]);
-        Penilaian::create([
-            'user_id' => '1',
-            'alternatif_id' => '4',
-            'slug' => 'Dell-P2422H',
-            'C1x' => '2',
-            'C2x' => '1',
-            'C3x' => '3',
-            'C4x' => '5',
-            'C5x' => '1',
-            'C6x' => '1',
-            'C7x' => '2',
-        ]);
-
-        Penilaian::create([
-            'user_id' => '1',
-            'alternatif_id' => '5',
-            'slug' => 'MSI-Optix-G273',
-            'C1x' => '2',
-            'C2x' => '2',
-            'C3x' => '3',
-            'C4x' => '5',
-            'C5x' => '2',
-            'C6x' => '4',
-            'C7x' => '4',
-        ]);
-
-        Penilaian::create([
-            'user_id' => '1',
-            'alternatif_id' => '6',
-            'slug' => 'Samsung-Odyssey-G5',
-            'C1x' => '2',
-            'C2x' => '2',
-            'C3x' => '3',
-            'C4x' => '3',
-            'C5x' => '2',
-            'C6x' => '4',
-            'C7x' => '1',
-        ]);
+        $this->call(PenilaianSeeder::class);
 
         // User::factory(1)->create();
     }
